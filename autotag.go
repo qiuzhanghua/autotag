cd1eb3f2ac81202ea7c2bd0e87b38bab2ec35fb1
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-28 08:17:56 +0800

    upgrade autotag version

--- autotag.go
+++ autotag.go
@@ -1,5 +1,5 @@
 package main
 
-var AppVersion = "v0.5.0"
-var AppRevision = "09a9699"
-var AppBuildDate = "2021-12-26"
+var AppVersion = "v0.5.2"
+var AppRevision = "32ce91c"
+var AppBuildDate = "2021-12-27"
